2c0f7d64798db202e7710da66b5173bca7f46106
Yashraj Rakibe <yashrajrakibe18@gmail.com>
2025-08-31 23:43:50 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,115 @@
+USE [Pizza DB]
+
+-- A) KPI'S
+
+SELECT * From pizza_sales;
+
+-- 1. Total Revenue:
+SELECT SUM(total_price) AS Total_Revenue from pizza_sales
+
+-- 2. Average Order Value
+SELECT SUM(total_price) / COUNT (DISTINCT order_id) AS Avg_order_Value From pizza_sales
+
+-- 3. Total Pizzas Sold
+SELECT Sum(quantity) AS Total_pizza_sold From pizza_sales
+
+-- 4. Total Orders
+SELECT Count(DISTINCT order_id) AS total_Orders FROM pizza_sales 
+
+-- 5. Average Pizzas Per Order
+SELECT CAST(CAST(SUM(quantity) AS Decimal(10, 2)) / 
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10, 2)) 
+AS Decimal(10, 2))
+AS Avg_pizza_per_order
+FROM pizza_sales
+
+
+
+
+SELECT * FROM pizza_sales
+
+-- B) Daily Trend for Total Orders
+SELECT DATENAME(DW, order_date) AS order_day, 
+COUNT(DISTINCT order_id) AS total_order
+FROM pizza_sales
+Group BY DATENAME(DW, order_date)
+
+-- C. Monthly Trend for Orders
+SELECT DATENAME(MONTH, order_date) AS Month_Name, 
+COUNT(DISTINCT order_id) AS Total_Order
+FROM pizza_sales
+GROUP BY DATENAME(MONTH, order_date)
+
+
+-- D. % of Sales by Pizza Category
+SELECT pizza_category, CAST (SUM(total_price) AS Decimal(10, 2)) AS total_revenue,
+CAST (SUM(total_price) * 100 / (SELECT SUM(total_price) 
+FROM pizza_sales) AS DECIMAL(10, 2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_category
+
+
+-- E. % of Sales by Pizza Size
+ SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10, 2)) AS total_revenue,
+ CAST(SUM(total_price) * 100 / ( Select SUM(total_price)
+ FROM pizza_sales) AS DECIMAL(10, 2)) AS PCT
+ FROM pizza_sales
+ GROUP BY pizza_size
+ ORDER BY pizza_size
+
+ -- F. Total Pizzas Sold by Pizza Category
+ SELECT pizza_category, SUM(quantity) AS Total_Quantity_sold
+ From pizza_sales
+-- WHERE MONTH(order_date) = 1          -- as Per Month It filter
+ GROUP BY pizza_category
+ ORDER BY Total_Quantity_sold DESC
+
+
+ -- G. Top 5 Pizzas by Revenue
+SELECT TOP 5 pizza_name, 
+SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue DESC
+
+
+-- H. Bottom 5 Pizzas by Revenue
+SELECT TOP 5 pizza_name, 
+SUM(total_price) AS Total_Revenue
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Revenue Asc
+
+
+
+-- I. Top 5 Pizzas by Quantity
+SELECT TOP 5 pizza_name, 
+SUM(quantity) AS Total_pizza_sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_pizza_sold DESC
+
+
+-- J. Bottom 5 Pizzas by Quantity
+SELECT TOP 5 pizza_name, 
+SUM(quantity) AS Total_pizza_sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_pizza_sold ASC
+
+
+-- K. Top 5 Pizzas by Total Orders
+SELECT TOP 5 pizza_name,
+COUNT(DISTINCT order_id) AS Total_order
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_order DESC
+
+
+-- L. Borrom 5 Pizzas by Total Orders
+SELECT TOP 5 pizza_name,
+COUNT(DISTINCT order_id) AS Total_order
+FROM pizza_sales
+-- WHERE pizza_category='Classic'
+GROUP BY pizza_name
+ORDER BY Total_order ASC
